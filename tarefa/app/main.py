de8dc8318d0df0a04e26b9a3447060d035be6280
thiagomesUNB <thiago.luiz20000@gmail.com>
2021-08-05 02:44:08 -0300

    Atualizando o ambiente

--- tarefa/app/main.py
+++ tarefa/app/main.py
@@ -1,14 +1,28 @@
 import pickle
 from fastapi import FastAPI
+import os
+
+#Nao consegue achar o diretorio
+titanic = pickle.load(open('Titanic.pkl', 'rb'))
 
 app = FastAPI()
-@app.post('/model')
-## Coloque seu codigo na função abaixo
-def titanic(Sex:int):
-    with open('model/Titanic.pkl', 'rb') as fid: 
-        titanic = pickle.load(fid)
 
 
 @app.get('/model')
-def get():
-    return {'hello':'test'}
+## Coloque seu codigo na função abaixo
+async def titanic(Sex:int,Age:float,Lifeboat: int,Pclass:int):
+    # with open('Titanic.pkl', 'rb') as fid: 
+    
+    # titanic = pickle.load(fid)
+    titanic.predict([[Sex,Age,Lifeboat,Pclass]])
+        
+    return {
+	"survived": bool(titanic.predict([[Sex,Age,Lifeboat,Pclass]])),	
+	"message": "teste"
+}
+
+
+# @app.get('/model')
+# def get():
+#     return {'hello':'test'}
+
