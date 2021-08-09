d7695251c4fd65908f3cdf0006302d3b917daf93
thiagomesUNB <thiago.luiz20000@gmail.com>
2021-08-09 14:42:45 -0300

    Alterando Dockerfile

--- tarefa/codigo/main.py
+++ tarefa/codigo/main.py
@@ -7,19 +7,27 @@ app = FastAPI()
 @app.post('/model',status_code=200)
 ## Coloque seu codigo na função abaixo
 async def titanic(Sex:int,Age:float,Lifeboat: int,Pclass:int,response: Response):
-    with open('Titanic.pkl', 'rb') as fid: 
+    with open('codigo/model/Titanic.pkl', 'rb') as fid: 
     
         titanic = pickle.load(fid)
         
         response.status_code = status.HTTP_201_CREATED
+
+        y_pred = bool(titanic.predict([[Sex,Age,Lifeboat,Pclass][0]]))
+
+        if y_pred:
+            survived = 'sobreviveu'
+        else
+            survived = 'morreu'
+
     return {
-	"survived": bool(titanic.predict([[Sex,Age,Lifeboat,Pclass]])),
+	"survived": y_pred,
     "status": response.status_code,
-	"message": "Me Contrata Stefanini!! :)"
+	"message": survived
 }
 
 
-@app.get('/model')
-def get():
-    return {'hello':'test'}
+# @app.get('/model')
+# def get():
+#     return {'hello':'test'}
 
